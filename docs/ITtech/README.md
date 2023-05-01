8883f65b65056c462c4f68f15b9fea9acbb9c36b
pikapikapikaori <pikapikapi_kaori@icloud.com>
2023-05-01 15:54:39 +0800

    feat: 使请求的动态图片可以根据黑暗模式切换主题

--- docs/ITtech/README.md
+++ docs/ITtech/README.md
@@ -2,7 +2,7 @@
  * @Author: pikapikapikaori pikapikapi_kaori@icloud.com
  * @Date: 2023-04-28 22:04:18
  * @LastEditors: pikapikapikaori pikapikapi_kaori@icloud.com
- * @LastEditTime: 2023-05-01 15:01:48
+ * @LastEditTime: 2023-05-01 15:25:08
  * @FilePath: /pikapikapi-blog/docs/ITtech/README.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -12,7 +12,7 @@
 
 ## 语言使用情况
 
-![picture](https://github-readme-stats.vercel.app/api/top-langs/?username=pikapikapikaori&langs_count=10&layout=compact)
+![picture](https://github-readme-stats.vercel.app/api/top-langs/?username=pikapikapikaori&langs_count=10&layout=compact&theme=buefy ':class=dynamicPictureAccordingToThemeMode')
 
 ## 一些小工具或项目
 
--- docs/utils/switchLightDarkMode.js
+++ docs/utils/switchLightDarkMode.js
@@ -2,13 +2,14 @@
  * @Author: pikapikapikaori pikapikapi_kaori@icloud.com
  * @Date: 2023-04-30 12:57:52
  * @LastEditors: pikapikapikaori pikapikapi_kaori@icloud.com
- * @LastEditTime: 2023-05-01 14:52:51
+ * @LastEditTime: 2023-05-01 15:47:01
  * @FilePath: /pikapikapi-blog/docs/utils/countWords.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 //default values
 var switchLightDarkModeOptions = {
     useSwitchMode: true,
+    switchDynamicPicture: false
 }
 
 // Docsify plugin functions
@@ -39,9 +40,43 @@ function plugin(hook, vm) {
             switchSpan.innerHTML = currentThemeMode ? lightModeIconHtml : darkModeIconHtml
             lightTheme.disabled = !currentThemeMode
             darkTheme.disabled = currentThemeMode
+
+            if (!switchLightDarkModeOptions.switchDynamicPicture) return
+
+            Array.from(document.getElementsByClassName('dynamicPictureAccordingToThemeMode')).forEach(img => {
+                var imgSrc = img.src
+
+                var themeParam = currentThemeMode ? 'buefy' : 'material-palenight'
+    
+                if (imgSrc.indexOf('theme=') > -1) {
+                    img.src = imgSrc.split('theme=')[0] + 'theme=' + themeParam
+                }
+                else {
+                    img.src = imgSrc + '&theme=' + themeParam
+                }
+            })
         }
 
         document.body.appendChild (switchSpan)
+    })
+
+    hook.afterEach(function(html, next) {
+        next(html);
+        
+        if (!switchLightDarkModeOptions.switchDynamicPicture) return
+
+        Array.from(document.getElementsByClassName('dynamicPictureAccordingToThemeMode')).forEach(img => {
+            var imgSrc = img.src
+
+            var themeParam = currentThemeMode ? 'buefy' : 'material-palenight'
+
+            if (imgSrc.indexOf('theme=') > -1) {
+                img.src = imgSrc.split('theme=')[0] + 'theme=' + themeParam
+            }
+            else {
+                img.src = imgSrc + '&theme=' + themeParam
+            }
+        })
     })
 }
 
