a27fff51720dcbd9bc81db3cfb8fbfaa7535ee51
pikapikapikaori <pikapikapi_kaori@icloud.com>
2023-05-05 00:16:07 +0800

    refactor: refactor code

--- docs/utils/switchLightDarkMode.js
+++ docs/utils/switchLightDarkMode.js
@@ -126,7 +126,7 @@ function plugin(hook, vm) {
         showWidgetsSpan.id = 'showWidgetsSpan'
         showWidgetsSpan.className = 'pageRightToolsWidgetsSpan'
         showWidgetsSpan.style.position = 'fixed'
-        showWidgetsSpan.style.right = '26px'
+        showWidgetsSpan.style.right = switchLightDarkModeOptions.right.toString() + 'px'
         showWidgetsSpan.style.top = (switchLightDarkModeOptions.top + 175).toString() + 'px'
         showWidgetsSpan.innerHTML = '<?xml version="1.0" encoding="UTF-8"?><svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="#d1b5ef"><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" stroke="#d1b5ef" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 15a3 3 0 100-6 3 3 0 000 6z" stroke="#d1b5ef" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M2 12h7M15 12h7" stroke="#d1b5ef" stroke-width="1.5"></path></svg>'
 
