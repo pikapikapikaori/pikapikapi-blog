b43cc4e9cb3bfb504aa742828b501ddd7d972fe0
pikapikapikaori <pikapikapi_kaori@icloud.com>
2023-05-08 18:12:53 +0800

    refactor: refactor view full image plugin

--- docs/utils/viewFullImage.js
+++ docs/utils/viewFullImage.js
@@ -14,80 +14,74 @@ function plugin(hook, vm) {
 
         viewFullImageSpan.id = 'viewFullImageSpan'
 
-        viewFullImageSpan.onclick = function (e) {
-            let viewFullImageLeftButtonSpan = document.getElementById('viewFullImageLeftButtonSpan')
-            let viewFullImageRightButtonSpan = document.getElementById('viewFullImageRightButtonSpan')
-            viewFullImageLeftButtonSpan.style.display = 'none'
-            viewFullImageRightButtonSpan.style.display = 'none'
-            this.style.display = 'none'
-        }
+        var viewFullImageSpanInnerLeftDiv = document.createElement('div')
+        var viewFullImageSpanInnerRightDiv = document.createElement('div')
 
-        var viewFullImageLeftButtonSpan = document.createElement('span')
-        viewFullImageLeftButtonSpan.id = 'viewFullImageLeftButtonSpan'
-        var viewFullImageRightButtonSpan = document.createElement('span')
-        viewFullImageRightButtonSpan.id = 'viewFullImageRightButtonSpan'
+        viewFullImageSpanInnerLeftDiv.id = 'viewFullImageSpanInnerLeftDiv'
+        viewFullImageSpanInnerRightDiv.id = 'viewFullImageSpanInnerRightDiv'
+        viewFullImageSpanInnerLeftDiv.innerHTML = '<?xml version="1.0" encoding="UTF-8"?><svg width="20px" height="20px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#c6a2eb"><path d="M15 6l-6 6 6 6" stroke="#c6a2eb" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
+        viewFullImageSpanInnerRightDiv.innerHTML = '<?xml version="1.0" encoding="UTF-8"?><svg width="20px" height="20px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#c6a2eb"><path d="M9 6l6 6-6 6" stroke="#c6a2eb" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
 
-        viewFullImageLeftButtonSpan.innerHTML = '<div class="viewFullImageButtonSpanInnerUpperLowerDiv" id="viewFullImageLeftButtonSpanInnerUpperDiv"></div><div id="viewFullImageLeftButtonSpanInnerDiv"><div id="viewFullImageLeftButtonSVG"><?xml version="1.0" encoding="UTF-8"?><svg width="20px" height="20px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#c6a2eb"><path d="M15 6l-6 6 6 6" stroke="#c6a2eb" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></div></div><div class="viewFullImageButtonSpanInnerUpperLowerDiv" id="viewFullImageLeftButtonSpanInnerLowerDiv"></div>'
-        viewFullImageRightButtonSpan.innerHTML = '<div class="viewFullImageButtonSpanInnerUpperLowerDiv" id="viewFullImageRightButtonSpanInnerUpperDiv"></div><div id="viewFullImageRightButtonSpanInnerDiv"><div id="viewFullImageRightButtonSVG"><?xml version="1.0" encoding="UTF-8"?><svg width="20px" height="20px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#c6a2eb"><path d="M9 6l6 6-6 6" stroke="#c6a2eb" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg></div></div><div class="viewFullImageButtonSpanInnerUpperLowerDiv" id="viewFullImageRightButtonSpanInnerLowerDiv"></div>'
-        viewFullImageSpan.innerHTML = '<div id="viewFullImageSpanInnerUpperDiv"></div><div id="viewFullImageSpanInnerDiv"></div>'
+        var viewFullImageSpanInnerImgDiv = document.createElement('div')
 
-        document.body.appendChild(viewFullImageLeftButtonSpan)
-        document.body.appendChild(viewFullImageRightButtonSpan)
+        viewFullImageSpanInnerImgDiv.id = 'viewFullImageSpanInnerImgDiv'
+
+        var viewFullImageSpanInnerTextDiv = document.createElement('div')
+
+        viewFullImageSpanInnerTextDiv.id = 'viewFullImageSpanInnerTextDiv'
+
+        viewFullImageSpan.appendChild(viewFullImageSpanInnerLeftDiv)
+        viewFullImageSpan.appendChild(viewFullImageSpanInnerRightDiv)
+        viewFullImageSpan.appendChild(viewFullImageSpanInnerImgDiv)
+        viewFullImageSpan.appendChild(viewFullImageSpanInnerTextDiv)
+
         document.body.appendChild(viewFullImageSpan)
-
-        var viewFullImageLeftButtonSVG = document.getElementById('viewFullImageLeftButtonSVG')
-        var viewFullImageRightButtonSVG = document.getElementById('viewFullImageRightButtonSVG')
 
         // true for left button
         function buttonLeftRightOnClick(direction) {
-            let viewFullImageSpanInnerDiv = document.getElementById('viewFullImageSpanInnerDiv')
+            let viewFullImageSpanInnerImgDiv = document.getElementById('viewFullImageSpanInnerImgDiv')
+            let viewFullImageSpanInnerTextDiv = document.getElementById('viewFullImageSpanInnerTextDiv')
 
             let imgArray = Array.from(document.getElementsByTagName('img')).filter(img => {
                 return !(img.className.split(' ').indexOf('ignoreViewFullImageImg') > -1 || img.className.indexOf('emoji') > -1 || img.src.indexOf('avatars.githubusercontent') > -1)
             })
 
-            imgArray.some((img, index) => {
-                if (viewFullImageSpanInnerDiv.style.backgroundImage.indexOf(img.src) > -1) {
+            imgArray.some((img, index, arr) => {
+                if (viewFullImageSpanInnerImgDiv.style.backgroundImage.indexOf(img.src) > -1) {
                     newImgIndex = direction ? (index === 0 ? imgArray.length - 1 : index - 1) : (index === imgArray.length - 1 ? 0 : index + 1)
-                    viewFullImageSpanInnerDiv.style.backgroundImage = 'url(' + imgArray[newImgIndex].src + ')'
+                    viewFullImageSpanInnerImgDiv.style.backgroundImage = 'url(' + imgArray[newImgIndex].src + ')'
+                    viewFullImageSpanInnerTextDiv.innerHTML = (newImgIndex + 1).toString() + ' / ' + arr.length.toString()
                     return true
                 }
             })
         }
 
-        viewFullImageLeftButtonSVG.onclick = function () {
-            buttonLeftRightOnClick(true)
-        }
-        viewFullImageRightButtonSVG.onclick = function () {
-            buttonLeftRightOnClick(false)
+        let notPreventParentOnClickEventElementId = [ viewFullImageSpanInnerImgDiv.id, viewFullImageSpan.id, viewFullImageSpanInnerTextDiv.id, ]
+
+        viewFullImageSpan.onclick = function (e) {
+            if (notPreventParentOnClickEventElementId.indexOf(e.target.id) === -1) return
+            this.style.display = 'none'
         }
 
-        Array.from(document.getElementsByClassName('viewFullImageButtonSpanInnerUpperLowerDiv')).forEach(viewFullImageButtonSpanInnerUpperLowerDiv => {
-            viewFullImageButtonSpanInnerUpperLowerDiv.onclick = function (e) {
-                let viewFullImageLeftButtonSpan = document.getElementById('viewFullImageLeftButtonSpan')
-                let viewFullImageRightButtonSpan = document.getElementById('viewFullImageRightButtonSpan')
-                let viewFullImageSpan = document.getElementById('viewFullImageSpan')
-                viewFullImageLeftButtonSpan.style.display = 'none'
-                viewFullImageRightButtonSpan.style.display = 'none'
-                viewFullImageSpan.style.display = 'none'
-            }
-        })
+        viewFullImageSpanInnerLeftDiv.onclick = function (e) {
+            buttonLeftRightOnClick(true)
+        }
+        viewFullImageSpanInnerRightDiv.onclick = function (e) {
+            buttonLeftRightOnClick(false)
+        }
     })
 
     hook.doneEach(function () {
-        Array.from(document.getElementsByTagName('img')).forEach(img => {
+        Array.from(document.getElementsByTagName('img')).filter(img => {
+            return !(img.className.split(' ').indexOf('ignoreViewFullImageImg') > -1 || img.className.indexOf('emoji') > -1 || img.src.indexOf('avatars.githubusercontent') > -1)
+        }).forEach((img, index, arr) => {
             let viewFullImageSpan = document.getElementById('viewFullImageSpan')
-            let viewFullImageLeftButtonSpan = document.getElementById('viewFullImageLeftButtonSpan')
-            let viewFullImageRightButtonSpan = document.getElementById('viewFullImageRightButtonSpan')
-            let viewFullImageSpanInnerDiv = document.getElementById('viewFullImageSpanInnerDiv')
-            if (img.className.split(' ').indexOf('ignoreViewFullImageImg') > -1 || img.className.indexOf('emoji') > -1 || img.src.indexOf('avatars.githubusercontent') > -1) {
-                return
-            }
+            let viewFullImageSpanInnerImgDiv = document.getElementById('viewFullImageSpanInnerImgDiv')
+            let viewFullImageSpanInnerTextDiv = document.getElementById('viewFullImageSpanInnerTextDiv')
             img.addEventListener('click', function () {
-                viewFullImageSpanInnerDiv.style.backgroundImage = 'url(' + img.src + ')'
+                viewFullImageSpanInnerImgDiv.style.backgroundImage = 'url(' + img.src + ')'
                 viewFullImageSpan.style.display = 'block'
-                viewFullImageLeftButtonSpan.style.display = 'block'
-                viewFullImageRightButtonSpan.style.display = 'block'
+                viewFullImageSpanInnerTextDiv.innerHTML = (index + 1).toString() + ' / ' + arr.length.toString()
             })
         })
     })
