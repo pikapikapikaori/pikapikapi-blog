268be89ad1e9cde05968932eeae89c75418b000b
pikapikapi <pikapikapi_kaori@icloud.com>
2024-03-13 20:11:36 +0800

    feat: add theme hue change

--- docs/utils/switchLightDarkMode.js
+++ docs/utils/switchLightDarkMode.js
@@ -167,12 +167,53 @@ function plugin(hook, vm) {
         document.body.appendChild(zoomOutSpan)
         document.body.appendChild(zoomDefaultSpan)
 
+        var colorPickerSpan = document.createElement('span')
+        colorPickerSpan.id = 'colorPickerSpan'
+        colorPickerSpan.className = 'pageRightToolsWidgetsSpan'
+        colorPickerSpan.style.position = 'fixed'
+        colorPickerSpan.style.right = switchLightDarkModeOptions.right.toString() + 'px'
+        colorPickerSpan.style.top = (switchLightDarkModeOptions.top + 140).toString() + 'px'
+        colorPickerSpan.innerHTML = '<?xml version="1.0" encoding="UTF-8"?><svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="var(--theme-color,#ea6f5a)"><path d="M7 13.161L12.4644 7.6966C12.8549 7.30607 13.4881 7.30607 13.8786 7.6966L15.9999 9.81792C16.3904 10.2084 16.3904 10.8416 15.9999 11.2321L14.0711 13.161M7 13.161L4.82764 15.3334C4.73428 15.4267 4.66034 15.5376 4.61007 15.6597L3.58204 18.1563C3.07438 19.3892 4.30728 20.6221 5.54018 20.1145L8.03681 19.0865C8.1589 19.0362 8.26981 18.9622 8.36317 18.8689L14.0711 13.161M7 13.161H14.0711" stroke="var(--theme-color,#ea6f5a)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M13.878 3.45401L15.9993 5.57533M20.242 9.81798L18.1206 7.69666M15.9993 5.57533L17.4135 4.16112C17.8041 3.7706 18.4372 3.7706 18.8277 4.16112L19.5349 4.86823C19.9254 5.25875 19.9254 5.89192 19.5349 6.28244L18.1206 7.69666M15.9993 5.57533L18.1206 7.69666" stroke="var(--theme-color,#ea6f5a)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
+
+        var colorPickerPopupSpan = document.createElement('span')
+        colorPickerPopupSpan.id = 'colorPickerPopupSpan'
+        colorPickerPopupSpan.className = 'colorPickerPopupSpan'
+        colorPickerPopupSpan.style.position = 'fixed'
+        colorPickerPopupSpan.style.right = (switchLightDarkModeOptions.right + 35).toString() + 'px'
+        colorPickerPopupSpan.style.top = (switchLightDarkModeOptions.top + 10).toString() + 'px'
+        var colorPickerPopupDiv = document.createElement('div')
+        colorPickerPopupDiv.id = 'colorPickerPopupDiv'
+        colorPickerPopupDiv.className = 'colorPickerPopupDiv'
+        colorPickerPopupDiv.innerHTML = '<input type="range" min="0" max="360" value="270" class="colorPickerSlider" id="colorPickerSlider" step="5">'
+
+        colorPickerPopupSpan.appendChild(colorPickerPopupDiv)
+
+        document.body.appendChild(colorPickerSpan)
+        document.body.appendChild(colorPickerPopupSpan)
+
+        colorPickerSlider = colorPickerPopupDiv.getElementsByClassName('colorPickerSlider')[0]
+
+        colorPickerSlider.oninput = function () {
+            document.documentElement.style.setProperty('--theme-color', hslToHex(this.value, 66, 78));
+        }
+
+        function hslToHex(h, s, l) {
+            l /= 100;
+            const a = s * Math.min(l, 1 - l) / 100;
+            const f = n => {
+                const k = (n + h / 30) % 12;
+                const color = l - a * Math.max(Math.min(k - 3, 9 - k, 1), -1);
+                return Math.round(255 * color).toString(16).padStart(2, '0');   // convert to Hex and prefix "0" if needed
+            };
+            return `#${f(0)}${f(8)}${f(4)}`;
+        }
+
         var scrollToCommentSpan = document.createElement('span')
         scrollToCommentSpan.id = 'scrollToCommentSpan'
         scrollToCommentSpan.className = 'pageRightToolsWidgetsSpan'
         scrollToCommentSpan.style.position = 'fixed'
         scrollToCommentSpan.style.right = switchLightDarkModeOptions.right.toString() + 'px'
-        scrollToCommentSpan.style.top = (switchLightDarkModeOptions.top + 140).toString() + 'px'
+        scrollToCommentSpan.style.top = (switchLightDarkModeOptions.top + 175).toString() + 'px'
         scrollToCommentSpan.innerHTML = '<?xml version="1.0" encoding="UTF-8"?><svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="var(--theme-color,#ea6f5a)"><path d="M17 12.5a.5.5 0 100-1 .5.5 0 000 1zM12 12.5a.5.5 0 100-1 .5.5 0 000 1zM7 12.5a.5.5 0 100-1 .5.5 0 000 1z" fill="var(--theme-color,#ea6f5a)" stroke="var(--theme-color,#ea6f5a)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12c0 1.821.487 3.53 1.338 5L2.5 21.5l4.5-.838A9.955 9.955 0 0012 22z" stroke="var(--theme-color,#ea6f5a)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
 
         scrollToCommentSpan.onclick = function () {
@@ -188,7 +229,7 @@ function plugin(hook, vm) {
         showSakuraSpan.className = 'pageRightToolsWidgetsSpan'
         showSakuraSpan.style.position = 'fixed'
         showSakuraSpan.style.right = switchLightDarkModeOptions.right.toString() + 'px'
-        showSakuraSpan.style.top = (switchLightDarkModeOptions.top + 175).toString() + 'px'
+        showSakuraSpan.style.top = (switchLightDarkModeOptions.top + 210).toString() + 'px'
         showSakuraSpan.innerHTML = '<?xml version="1.0" encoding="UTF-8"?><svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="var(--theme-color,#ea6f5a)"><path d="M12 15a3 3 0 100-6 3 3 0 000 6zM13 9s1-2 1-4-2-4-2-4-2 2-2 4 1 4 1 4" stroke="var(--theme-color,#ea6f5a)" stroke-width="1.5" stroke-miterlimit="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M9 11s-2-1-4-1-4 2-4 2 2 2 4 2 4-1 4-1M13 15s1 2 1 4-2 4-2 4-2-2-2-4 1-4 1-4M15 11s2-1 4-1 4 2 4 2-2 2-4 2-4-1-4-1M10.586 9.172S9.879 7.05 8.464 5.636C7.05 4.222 4.222 4.222 4.222 4.222s0 2.828 1.414 4.243c1.414 1.414 3.536 2.121 3.536 2.121M9.172 13.414s-2.122.707-3.536 2.122c-1.414 1.414-1.414 4.242-1.414 4.242s2.828 0 4.242-1.414c1.415-1.414 2.122-3.536 2.122-3.536M14.829 13.414s2.12.707 3.535 2.122c1.414 1.414 1.414 4.242 1.414 4.242s-2.828 0-4.242-1.414c-1.415-1.414-2.122-3.536-2.122-3.536M13.414 9.172s.707-2.122 2.122-3.536c1.414-1.414 4.242-1.414 4.242-1.414s0 2.828-1.414 4.243c-1.414 1.414-3.536 2.121-3.536 2.121" stroke="var(--theme-color,#ea6f5a)" stroke-width="1.5" stroke-miterlimit="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>'
 
         let isSakuraDisplayed = true
@@ -211,13 +252,13 @@ function plugin(hook, vm) {
         showWidgetsSpan.className = 'pageRightToolsWidgetsSpan'
         showWidgetsSpan.style.position = 'fixed'
         showWidgetsSpan.style.right = switchLightDarkModeOptions.right.toString() + 'px'
-        showWidgetsSpan.style.top = (switchLightDarkModeOptions.top + 210).toString() + 'px'
+        showWidgetsSpan.style.top = (switchLightDarkModeOptions.top + 245).toString() + 'px'
         showWidgetsSpan.innerHTML = '<?xml version="1.0" encoding="UTF-8"?><svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none" xmlns="http://www.w3.org/2000/svg" color="var(--theme-color,#ea6f5a)"><path d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" stroke="var(--theme-color,#ea6f5a)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M12 15a3 3 0 100-6 3 3 0 000 6z" stroke="var(--theme-color,#ea6f5a)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M2 12h7M15 12h7" stroke="var(--theme-color,#ea6f5a)" stroke-width="1.5"></path></svg>'
 
         let isWidgetsOpen = true
 
         showWidgetsSpan.onclick = function () {
-            let widgetsSpanList = [switchSpan, zoomInSpan, zoomOutSpan, zoomDefaultSpan, scrollToCommentSpan, showSakuraSpan, ]
+            let widgetsSpanList = [switchSpan, zoomInSpan, zoomOutSpan, zoomDefaultSpan, colorPickerSpan, scrollToCommentSpan, showSakuraSpan, ]
 
             isWidgetsOpen = !isWidgetsOpen
 
