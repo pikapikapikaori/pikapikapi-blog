6999a743f3276e385fc985c736eddb7bed9c9a44
pikapikapikaori <pikapikapi_kaori@icloud.com>
2024-07-14 13:44:37 +0800

    refactor: refactor background with js

--- docs/utils/dynamicCover.js
+++ docs/utils/dynamicCover.js
@@ -1,9 +1,25 @@
 function plugin(hook, vm) {
+    let backgrounds = [
+        '_media/coverBackgrounds/cover-1.jpg',
+        '_media/coverBackgrounds/cover-2.jpg',
+        '_media/coverBackgrounds/cover-3.jpg',
+    ]
+
+    let numberOfImages = 3 // Change this to the number of background images
+
+    let interval = 8 * 1000
+
     hook.doneEach(function () {
         let cover = document.getElementsByClassName('cover')[0]
         cover.classList.add('dynamicCover')
         let mask = cover.getElementsByClassName('mask')[0]
         mask.style.opacity = .6
+
+        var curImgId = 1
+        window.setInterval(function () {
+            cover.style.background = 'url(' + backgrounds[curImgId] + ')' + ' center center / cover'
+            curImgId = (curImgId + 1) % numberOfImages
+        }, interval)
     })
 }
 
