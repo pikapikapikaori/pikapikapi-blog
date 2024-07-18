69274a290accbb3af73a4a5b3988e28c2253b814
pikapikapikaori <pikapikapi_kaori@icloud.com>
2024-07-18 16:40:40 +0800

    config: remove Sites & Beginning from rss

--- docs/rss.sh
+++ docs/rss.sh
@@ -17,7 +17,7 @@ urlencode() {
 }
 
 newest_files=$(
-    git ls-files -z '**/*.md' ':!:**/_*.md' ':!:**/README.md' ':!:**/About.md' ':!:**/Personal**.md' ':!:**/BriefComments.md' |
+    git ls-files -z '**/*.md' ':!:**/_*.md' ':!:**/README.md' ':!:**/About.md' ':!:**/Personal**.md' ':!:**/BriefComments.md' ':!:**/Beginning.md' ':!:**/Sites.md' |
         xargs -0 -n1 -I{} -- git log -1 --format="%at {}" {} |
         sort -r |
         head -n10 |
